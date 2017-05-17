664f09f15b9d178e2dacd1437a1cf0e30c511e70
DeliOmurice <aznforl1f3@hotmail.com>
2017-05-17 10:50:24 +0800

    signup fix 2

--- uber2/src/main/webapp/uber/app/view/login/SignupController.js
+++ uber2/src/main/webapp/uber/app/view/login/SignupController.js
@@ -10,53 +10,26 @@ Ext.define('uber.view.login.SignupController',{
     	var errors = model.validate();
     	var form = formPanel.getForm();
     	
-    	if (formPanel.isValid()){
-//    		var out = [];
-//    		Ext.Object.each(form.getValues(), function(key, value){
-//                out.push(key + ' = ' + value);
-//            });
-//            Ext.Msg.alert('Submitted Values', out.join('<br />'));
-            
-			form.submit({
-			//submit form for user signup
-				url: '/uber2/main/signup!save.action',
-				method: 'POST',
-				success: function() {
-					Ext.Msg.alert( '', 'registration success', Ext.emptyFn )
-				},
-		
-				failure: function() {
-					Ext.Msg.alert('', 'registration failure', Ext.emptyFn )
-				}
-			})
+    	if (model.isValid()){
+    		formPanel.submit({
+    			//submit form for user signup
+    			url: '/uber2/main/signup!signup.action',
+    			method: 'POST',
+    			success: function() {
+    				Ext.Msg.alert( '', 'registration success', Ext.emptyFn )
+    			},
+
+    			failure: function() {
+    				Ext.Msg.alert('', 'registration failure', Ext.emptyFn )
+    			},
+    		})
+    	} else {
+    		var message = "";
+    		Ext.each(errors.items, function(rec){
+    			message +=rec.getMessage()+"<br>"
+    		});
+    		Ext.Msg.alert("Error", message, Ext.emptyFn);
     	}
-    	
-//    	if (model.isValid()){
-////    		var out = [];
-////            Ext.Object.each(model.getValues(), function(key, value){
-////                out.push(key + '=' + value);
-////            });
-////            Ext.Msg.alert('Submitted Values', out.join('<br />'));
-//    		
-//    		formPanel.submit({
-//    			//submit form for user signup
-//    			url: '/uber2/main/signup!signup.action',
-//    			method: 'POST',
-//    			success: function() {
-//    				Ext.Msg.alert( '', 'registration success', Ext.emptyFn )
-//    			},
-//
-//    			failure: function() {
-//    				Ext.Msg.alert('', 'registration failure', Ext.emptyFn )
-//    			},
-//    		})
-//    	} else {
-//    		var message = "";
-//    		Ext.each(errors.items, function(rec){
-//    			message +=rec.getMessage()+"<br>"
-//    		});
-//    		Ext.Msg.alert("Error", message, Ext.emptyFn);
-//    	}
     }
 
 //	updateErrorState: function(cmp, state) {
