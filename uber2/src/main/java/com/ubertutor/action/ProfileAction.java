d24e5d2c7abb220d19e30ce9bda3fa0d787da2ba
Victor Sun <mynameisvictor@126.com>
2017-06-02 20:47:54 +0800

    V

--- uber2/src/main/java/com/ubertutor/action/ProfileAction.java
+++ uber2/src/main/java/com/ubertutor/action/ProfileAction.java
@@ -1,8 +1,15 @@
 package com.ubertutor.action;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.struts2.convention.annotation.Namespace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springside.modules.utils.web.struts2.Struts2Utils;
 
+import com.gnomon.common.utils.JsonResult;
 import com.gnomon.common.web.SessionData;
 import com.opensymphony.xwork2.ActionSupport;
 import com.ubertutor.entity.UserEntity;
@@ -18,12 +25,15 @@ public class ProfileAction extends ActionSupport{
 	public void display() throws Exception{
 		try{
 			UserEntity user = SessionData.getLoginUser();
-			System.out.println(user.getFullname());
-			System.out.println(user.getEmail());
-			System.out.println(user.getMobile());
-			System.out.println(user.getBio());
-//			System.out.println(profileService.getSchool((long)2).getName());
-			System.out.println(user.getSchools());
+			JsonResult result = new JsonResult();
+			Map<String, String> profileResult = new HashMap<String,String>();
+			profileResult.put("fullname",user.getFullname());
+			profileResult.put("email",user.getEmail());
+			profileResult.put("mobile",user.getMobile());
+			profileResult.put("bio",user.getBio());
+			profileResult.put("school",profileService.getSchool((long)2).getName());
+			System.out.println(profileResult.toString());
+//			Struts2Utils.renderJson(profileResult);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
