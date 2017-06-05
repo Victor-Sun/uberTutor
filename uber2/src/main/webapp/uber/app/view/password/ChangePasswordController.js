bda068da7e48192c80d4eab965527504b58e8ea1
Victor Sun <mynameisvictor@126.com>
2017-06-06 00:35:20 +0800

    Update Password

--- uber2/src/main/webapp/uber/app/view/password/ChangePasswordController.js
+++ uber2/src/main/webapp/uber/app/view/password/ChangePasswordController.js
@@ -9,7 +9,7 @@ Ext.define('uber.view.password.ChangePasswordController',{
     	if(formPanel.getForm().isValid()){
     		formPanel.submit({
     			//submit form for user signup
-    			url: '/uber2/main/signup!changePassword.action',
+    			url: '/uber2/main/password!updatePassword.action',
     			method: 'POST',
     			success: function() {
     				// change to exception output
