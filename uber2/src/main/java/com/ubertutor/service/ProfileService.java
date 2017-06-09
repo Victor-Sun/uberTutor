200ff049fd439e657c7d4afd43f62dad9fe95081
Victor Sun <sunvi@mail.gvsu.edu>
2017-06-09 15:08:25 +0800

    V

--- uber2/src/main/java/com/ubertutor/service/ProfileService.java
+++ uber2/src/main/java/com/ubertutor/service/ProfileService.java
@@ -114,7 +114,7 @@ public class ProfileService {
 	 * @return true if the email is valid
 	 */
 	public boolean isValidEmailAddress(String email) {
-		String pattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+		String pattern = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 		java.util.regex.Pattern p = java.util.regex.Pattern.compile(pattern);
 		java.util.regex.Matcher m = p.matcher(email);
 		return m.matches();
