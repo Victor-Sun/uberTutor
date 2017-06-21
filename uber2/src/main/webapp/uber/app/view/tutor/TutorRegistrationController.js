70ddbdf169f156d6c8b5d03b1a2da47a2cf48c93
DeliOmurice <aznforl1f3@hotmail.com>
2017-06-21 21:28:27 +0800

    E

--- uber2/src/main/webapp/uber/app/view/tutor/TutorRegistrationController.js
+++ uber2/src/main/webapp/uber/app/view/tutor/TutorRegistrationController.js
@@ -57,7 +57,7 @@ Ext.define('uber.view.tutor.TutorRegistrationController',{
     	Ext.Ajax.request({
     		url:'/uber2/main/tutor-subject-register!removeSubject.action',
     		params: {
-    			ID:record.data.ID
+    			userSubjectId:record.data.ID
     		},
     		score: me,
     		success: function() {
