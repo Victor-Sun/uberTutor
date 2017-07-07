c1419c049e4ca793fe750a78d65ab6687c9ba2fc
Victor Sun <sunvi@mail.gvsu.edu>
2017-07-07 13:21:01 +0800

    V

--- UberTutor/src/main/java/com/ubertutor/action/FeedbackAction.java
+++ UberTutor/src/main/java/com/ubertutor/action/FeedbackAction.java
@@ -11,7 +11,7 @@ import com.ubertutor.entity.FeedbackEntity;
 import com.ubertutor.service.FeedbackService;
 
 @Namespace("/main")
-@AllowedMethods("save")
+@AllowedMethods({"save","displayFeedbackInfo"})
 public class FeedbackAction extends PDMSCrudActionSupport<FeedbackEntity> {
 	private static final long serialVersionUID = 1L;
 	private FeedbackEntity feedbackEntity;
